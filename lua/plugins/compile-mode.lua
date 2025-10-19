5ab7ee8f20ddfd04f3aac67334b4a041a2e49a5f
llem00n <alex.porubaimikh@outlook.com>
2025-10-19 10:36:14 +0300

    add compile mode

---
+++ lua/plugins/compile-mode.lua
@@ -0,0 +1,17 @@
+return {
+  "ej-shafran/compile-mode.nvim",
+  branch = "latest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "m00qek/baleia.nvim", tag = "v1.3.0" },
+  },
+  keys = {
+    { "<C-\\><C-\\>", "<CMD>Compile<CR>", mode = "n", desc = "Compile mode" }
+  },
+  config = function()
+    vim.g.compile_mode = {
+      baleia_setup = true,
+      bang_expansion = true,
+    }
+  end
+}
